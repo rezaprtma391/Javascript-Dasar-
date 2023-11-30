67a160cde0f06686e2cf7e14829bb3aee970ffa1
Muhamad Reza Pratama <rezaprtma391@gmail.com>
2023-11-30 16:49:33 +0700

    second-commit

---
+++ coba.js
@@ -0,0 +1,19 @@
+// Tools Js
+// alert("Assalamualaikum!");
+// prompt("1 === 1 Adalah");
+// console.log("WKWKWKWKWKW");
+// console.warn("Warning Bukan Artinya Warna Kuning Ya!!!!")
+// console.error("Yah Kok Error XIXiXIXIXI")
+
+// Belajar If
+// let bilangan = 8;
+// if (bilangan % 2 === 0) {
+//     console.log(bilangan,"adalah bilangan genap")
+// } else {
+//     console.log(bilangan,"adalah bilangan ganjil")
+// };
+
+// let nama = prompt('Masukkan Nama Anda: ', 'Yang Bener!'); 
+// if (nama === '') { 
+//     alert('Anda belum memasukkan nama')
+// }
--- index.js
+++ index.js
@@ -12,26 +12,96 @@ let x = 0;
 
 for(x = 0; x <= 10; x++) {
     console.log("Angka Ke", x)
-}
+};
 
 let count = 0;
 
 while (count <= 10) {
  console.log("Angka Ke", count);
  count++;
-}
+};
 
 //function
 //Pakai Console.log
 function myFunction(x, y) {
     console.log(x * y)
-}
+};
 
 myFunction(5, 5)
 //Pakai Return
 let add = function(a, b) {
     return a + b;
-    };
+};
 
 let hasil = add(1, 2);
-console.log(hasil)
+console.log(hasil);
+
+//Method Math
+console.log(Math.PI);
+console.log(Math.random());
+console.log(Math.abs(-123));
+console.log(Math.round(5.9));
+console.log(Math.floor(5.9));
+console.log(Math.cos(1));
+
+//Logika Perbadingan
+console.log(5 >= 5, 5 > 5);
+console.log(5 == '5', 5 === '5');
+console.log(5 != 5, 5 !== '5');
+
+//Bilangan Ganjil-Genap
+let bilangan = 8;
+if (bilangan % 2 === 0) {
+    console.log(bilangan, "adalah bilangan genap")
+} else {
+    console.log(bilangan, "adalah bilangan ganjil")
+};
+
+// Belajar IF
+let angka = Math.random();
+console.log("Ini adalah angkanya", angka);
+if (angka >= 0.5) {
+    console.log("Angka lebih besar dari angka 0.5")
+};
+if (angka <= 0.5) {
+    console.log("Angka lebih kecil dari angka 0.5")
+};
+
+//Belajar Else IF 
+let Hari = "Sabtu";
+if (Hari === "Senin") {
+    console.log("Anda berada di hari Senin");
+}
+else if (Hari === "Sabtu") {
+    console.log("Anda berada di hari sabtu")
+};
+
+let nilai = 49;
+if (nilai > 90) {
+    console.log("A");
+}
+else if (nilai > 70) {
+    console.log("B")
+}
+else if (nilai > 60) {
+    console.log("C")
+}
+else if (nilai > 50) {
+    console.log("E")
+}
+else if (nilai < 50) {
+    console.log("D")
+}
+
+//Belajar Else
+let hariHari = "Selasa";
+
+if (hariHari === "Senin") {
+    console.log("Anda berada di hari Senin");
+}
+else if (hariHari === "Sabtu") {
+    console.log("Anda berada di hari sabtu")
+}
+else {
+    console.log("Selain Senin Dan Sabtu")
+};
